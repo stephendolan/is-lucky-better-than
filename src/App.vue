b45fbc2f75fdd2f034321cce8aaa39d37c6fa154
Stephen Dolan <dolan.stephen1@gmail.com>
2021-02-28 01:37:29 -0500

    Good stopping point

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="h-screen bg-gray-600">
+    <Navbar />
+
+    <div class="px-10 py-10 grid sm:grid-cols-3 md:grid-cols-4 gap-6">
+      <FrameworkComparison v-for="framework in frameworks" v-bind="framework" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import Navbar from "./components/Navbar.vue";
+import FrameworkComparison from "./components/FrameworkComparison.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-})
+    Navbar,
+    FrameworkComparison,
+  },
+  data: () => {
+    return {
+      frameworks: [
+        {
+          name: "Amber",
+          language: "crystal",
+          website: "https://amberframework.org",
+          github: "https://github.com/amberframework/amber",
+          logo: "amber.jpg",
+        },
+        {
+          name: "Kemal",
+          language: "crystal",
+          website: "https://kemalcr.com",
+          github: "https://github.com/kemalcr/kemal",
+          logo: "kemal.png",
+        },
+        {
+          name: "Rails",
+          language: "ruby",
+          website: "https://rubyonrails.org",
+          github: "https://github.com/rails/rails",
+          logo: "rails.png",
+        },
+        {
+          name: "Phoenix",
+          language: "Elixir",
+          website: "https://www.phoenixframework.org",
+          github: "https://github.com/phoenixframework/phoenix",
+          logo: "phoenix.png",
+        },
+      ],
+    };
+  },
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="bg-gray-800 py-4">
+    <h1 class="text-center text-white text-2xl font-bold">
+      Is Lucky better than...
+    </h1>
+  </nav>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "Navbar",
+});
+</script>
+
+<style scoped></style>
